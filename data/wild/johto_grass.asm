b57a25e9c472bd6b6727066a49e188137f01a18a
cilerba <cilerba@users.noreply.github.com>
2021-09-13 17:46:16 -0400

    Add a few level 4's to ROUTE 29

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1239,26 +1239,26 @@ JohtoGrassWildMons:
 	; morn
 	db 2, PIDGEY
 	db 2, SENTRET
-	db 3, PIDGEY
+	db 4, PIDGEY
 	db 3, SENTRET
-	db 2, RATTATA
+	db 4, RATTATA
 	db 3, HOPPIP
 	db 3, HOPPIP
 	; day
 	db 2, PIDGEY
 	db 2, SENTRET
 	db 3, PIDGEY
-	db 3, SENTRET
-	db 2, RATTATA
+	db 4, SENTRET
+	db 3, RATTATA
 	db 3, HOPPIP
 	db 3, HOPPIP
 	; nite
-	db 2, HOOTHOOT
+	db 3, HOOTHOOT
 	db 2, RATTATA
 	db 3, HOOTHOOT
-	db 3, RATTATA
+	db 4, RATTATA
 	db 2, RATTATA
-	db 3, HOOTHOOT
+	db 4, HOOTHOOT
 	db 3, HOOTHOOT
 	end_grass_wildmons
 
