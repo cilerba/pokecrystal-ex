41b1aeab90499946cae57406ed73b862f07570c8
cilerba <maxdcordeiro@gmail.com>
2022-04-15 15:24:28 -0400

    Remove item effects from PokePager, forcing it to be used through button combo

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -158,7 +158,7 @@ ItemEffects:
 	dw NoEffect            ; ITEM_8E
 	dw NoEffect            ; METAL_COAT
 	dw NoEffect            ; DRAGON_FANG
-	dw PokePagerEffect     ; ITEM_91
+	dw NoEffect     ; ITEM_91
 	dw NoEffect            ; LEFTOVERS
 	dw NoEffect            ; ITEM_93
 	dw NoEffect            ; ITEM_94
